c01a92f32fc01c3e9a7f96bcc66173184e54fef1
ushiu <ushiushi2018@gmail.com>
2025-09-07 16:42:34 +0900

    作業中　その２

---
+++ data/src/Common/Geometry.h
@@ -0,0 +1,79 @@
+#pragma once
+
+// -------------
+// -- Vector2 --
+// -------------
+struct Vector2 {
+	Vector2() : x(0), y(0) {}
+	Vector2(float nx, float ny) : x(nx), y(ny) {}
+
+	float x, y;
+
+	const float Magnitude() const;
+
+	void Normalize();
+	Vector2 Normalized() const;
+
+	void operator+=(const Vector2& v);
+	void operator-=(const Vector2& v);
+	void operator*=(float scale);
+	Vector2 operator*(float scale) const;
+	Vector2 operator-() const;
+};
+
+Vector2 operator+(const Vector2& va, const Vector2 vb);
+Vector2 operator-(const Vector2& va, const Vector2 vb);
+
+float Dot(const Vector2& va, const Vector2& vb);
+float Cross(const Vector2& va, const Vector2& vb);
+
+float operator*(const Vector2& va, const Vector2& vb);
+float operator%(const Vector2& va, const Vector2& vb);
+
+// -------------
+// -- Vector3 --
+// -------------
+struct Vector3 {
+	Vector3() : x(0), y(0), z(0) {}
+	Vector3(float nx, float ny, float nz) : x(nx), y(ny), z(nz) {}
+
+	float x, y, z;
+
+	float Magnitude() const;
+	float SQMagnitude() const;
+
+	void Normalize();
+	Vector3 Normalized() const;
+
+	void operator+=(const Vector3& v);
+	void operator-=(const Vector3& v);
+	void operator*=(float scale);
+	Vector3 operator*(float scale) const;
+	Vector3 operator-() const;
+};
+
+float Dot(const Vector3& va, const Vector3& vb);
+Vector3 Cross(const Vector3& va, const Vector3& vb);
+
+float operator*(const Vector3& va, const Vector3& vb);
+Vector3 operator%(const Vector3& va, const Vector3& vb);
+
+// ---------------
+// -- Matrix4x4 --
+// ---------------
+struct Matrix4x4 {
+	float m[4][4] = {};
+
+	Matrix4x4 operator*(const Matrix4x4& rhs) const;
+	Vector3 operator*(const Vector3& v) const;
+};
+
+Matrix4x4 IdentityMatrix();
+Matrix4x4 TransposeMatrix(const Matrix4x4& m);
+
+Matrix4x4 RotationMatrixX(float angle);
+Matrix4x4 RotationMatrixY(float angle);
+Matrix4x4 RotationMatrixZ(float angle);
+
+Matrix4x4 TranslationMatrix(const Vector3& v);
+Matrix4x4 TranslationMatrix(float x, float y, float z);
---
+++ data/src/Scene/Game/Cube.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <array>
+#include <DxLib.h>
+#include "../../Common/Geometry.h"
+
+struct BoundingBox { //ダイスの最小最大の座標
+	Vector3 min;
+	Vector3 max;
+};
+
+class Cube {
+private:
+	int imgHandle_;
+	std::array<VERTEX3D, 24> vertices_;
+	std::array<uint16_t, 36> indices_;
+
+public:
+	Cube();
+
+	void Update(const Matrix4x4& mat);
+	void Draw() const;
+
+	BoundingBox GetBoundingBox() const;
+
+};
