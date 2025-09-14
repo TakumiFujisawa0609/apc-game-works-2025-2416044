2a9b8bdd2bfb1a2745333c5171d7216621770fb8
ushiu <ushiushi2018@gmail.com>
2025-09-15 00:11:29 +0900

    作業中　その３

--- file/src/Common/Geometry.cpp
+++ file/src/Common/Geometry.cpp
@@ -148,9 +148,9 @@ Matrix4x4 Matrix4x4::operator*(const Matrix4x4& rhs) const {
 
 Vector3 Matrix4x4::operator*(const Vector3& v) const {
 	Vector3 result = {};
-	result.x = m[0][0] * v.x + m[0][1] * v.x + m[0][2] * v.x + m[0][3];
-	result.y = m[1][0] * v.y + m[1][1] * v.y + m[1][2] * v.y + m[1][3];
-	result.z = m[2][0] * v.z + m[2][1] * v.z + m[2][2] * v.z + m[2][3];
+	result.x = m[0][0] * v.x + m[0][1] * v.y + m[0][2] * v.z + m[0][3];
+	result.y = m[1][0] * v.x + m[1][1] * v.y + m[1][2] * v.z + m[1][3];
+	result.z = m[2][0] * v.x + m[2][1] * v.y + m[2][2] * v.z + m[2][3];
 	return result;
 }
 
